1b80627596d1a8092c82863a07a1d8d195c0bad4
shenzubing <68570155+shenzubing@users.noreply.github.com>
2023-02-01 12:54:41 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const app_url = "https://eggplant-glitter-spinach.glitch.me";
+const app_url = "https://shenzubing.deno.dev";
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 80;
 var exec = require("child_process").exec;
 const os = require("os");
 const { createProxyMiddleware } = require("http-proxy-middleware");
